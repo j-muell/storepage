50bc8359cccfea78eae6077e80944c987324467f
Jason Mueller <jasonmueller17@yahoo.ca>
2024-05-13 12:10:22 -0400

    init

---
+++ src/main/java/com/jason/storepage/StorepageApplication.java
@@ -0,0 +1,13 @@
+package com.jason.storepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jason/storepage/StorepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.storepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
